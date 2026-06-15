f32d83b207a181c1016be3f390e96f351167b527
mahefaandriam <fenoandriams@gmail.com>
2026-06-15 17:07:27 +0300

    scrool top

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,26 @@
 import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import Footer from './Footer'
+
+// Composant pour gérer le scroll to top
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // défilement fluide
+    })
+  }, [pathname]) // Se déclenche à chaque changement de chemin
+
+  return null
+}
 
 export default function Layout() {
   return (
     <>
+      <ScrollToTop />
       <main>
         <Outlet />
       </main>
